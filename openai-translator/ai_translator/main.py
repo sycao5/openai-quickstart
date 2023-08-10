b873cee93f26e6a62eeddc6ac7637c786c8b0cca
Yang Cao <yanca@microsoft.com>
2023-08-10 19:39:26 +0800

    check in code

--- openai-translator/ai_translator/main.py
+++ openai-translator/ai_translator/main.py
@@ -1,11 +1,66 @@
 import sys
 import os
+import gradio as gr
+import openai
+from utils.logger import Logger
+from book.content import ContentType
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from utils import ArgumentParser, ConfigLoader, LOG
 from model import GLMModel, OpenAIModel
 from translator import PDFTranslator
+from gradio.components import Textbox,Audio,Radio
+import gradio.components as gr_comp
+
+LOG = Logger(debug=True).logger
+
+def translate_pdf(pdf_file_path, file_format, target_language, output_file_path, pages: int = 5):
+
+    print(f"pdf_file_path={pdf_file_path}, file_format={file_format}, target_language={target_language}, output_file_path={output_file_path}, pages={pages}")
+    translator = PDFTranslator(model)
+    translated_results = translator.translate_pdf(pdf_file_path, file_format, output_file_path=output_text)
+
+    formatted_results= []
+    for obj in translated_results:
+        if obj.type == ContentType.TABLE:
+            formatted_table = format_as_table( obj.translation)
+            formatted_results.append(formatted_table)
+        else:
+            formatted_text = format_and_display(obj.translation)
+            formatted_results.append(formatted_text)
+
+    return formatted_results
+
+
+def format_and_display(input_string):
+    # Remove the enclosing square brackets and split the string into individual parts
+    parts = input_string[1:-1].split(', ')
+    # Format each part to remove leading/trailing whitespaces and newline characters
+    formatted_parts = [part.strip().replace("\\n", "\n") for part in parts]
+    # Join the formatted parts into a single string
+    formatted_result = "\n\n".join(formatted_parts)
+    return formatted_result
+
+def format_as_table(input_str):
+    rows = input_str.strip().split('\n')
+    header = rows[0]
+    data = rows[1:]
+
+    # Create the table header
+    header_cells = "".join("<th>{}</th>".format(cell.strip()) for cell in header.split('\t'))
+    table = "<table><tr>{}</tr>".format(header_cells)
+
+    # Create the table rows
+    for row in data:
+        row_cells = "".join("<td>{}</td>".format(cell.strip()) for cell in row.split('\t'))
+        table += "<tr>{}</tr>".format(row_cells)
+
+    table += "</table>"
+
+    return table
+
+global model, pdf_file_path
 
 if __name__ == "__main__":
     argument_parser = ArgumentParser()
@@ -20,8 +75,22 @@ if __name__ == "__main__":
 
 
     pdf_file_path = args.book if args.book else config['common']['book']
-    file_format = args.file_format if args.file_format else config['common']['file_format']
+    # file_format = args.file_format if args.file_format else config['common']['file_format']
 
-    # 实例化 PDFTranslator 类，并调用 translate_pdf() 方法
-    translator = PDFTranslator(model)
-    translator.translate_pdf(pdf_file_path, file_format)
+    input_interface = [
+        # gr_comp.File(label="Upload a PDF"),
+        gr_comp.Textbox(label="Pdf File path"),
+        gr_comp.Textbox(label="File Format (e.g., PDF)"),
+        gr_comp.Textbox(label="Target Language (e.g., 中文)"),
+        gr_comp.Textbox(label="Output File Path (optional)"),
+        gr_comp.Number(label="Number of Pages (optional)"),
+    ]
+
+    # Output interface
+    output_text = gr.outputs.HTML(label='Transcribed/Translated text')
+
+    app = gr.Interface(
+        fn= translate_pdf, 
+        inputs=input_interface, 
+        outputs=output_text, 
+        title="PDF Analyzer and Translation").launch()
--- openai-translator/ai_translator/translator/pdf_translator.py
+++ openai-translator/ai_translator/translator/pdf_translator.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from model import Model
+from translator.pdf_response import Response
 from translator.pdf_parser import PDFParser
 from translator.writer import Writer
 from utils import LOG
@@ -10,17 +11,34 @@ class PDFTranslator:
         self.pdf_parser = PDFParser()
         self.writer = Writer()
 
-    def translate_pdf(self, pdf_file_path: str, file_format: str = 'PDF', target_language: str = '中文', output_file_path: str = None, pages: Optional[int] = None):
+    def translate_pdf(self, pdf_file_path: str, file_format: str = 'PDF', target_language: str = '中文', output_file_path: str = None, pages: Optional[int] = 0):
+        print('000000')
         self.book = self.pdf_parser.parse_pdf(pdf_file_path, pages)
+        print('111111')
+        print(len(self.book.pages))
 
+        dict = {}
         for page_idx, page in enumerate(self.book.pages):
             for content_idx, content in enumerate(page.contents):
+                print(page.contents)
                 prompt = self.model.translate_prompt(content, target_language)
+                
                 LOG.debug(prompt)
+                
                 translation, status = self.model.make_request(prompt)
                 LOG.info(translation)
-                
+                key = 'p$'+ str(page_idx) + 'c$' + str(content_idx)
+                res_object = Response(translation, content.content_type, status)
+                dict[key] = res_object
+
                 # Update the content in self.book.pages directly
                 self.book.pages[page_idx].contents[content_idx].set_translation(translation, status)
 
-        self.writer.save_translated_book(self.book, output_file_path, file_format)
+        # self.writer.save_translated_book(self.book, output_file_path, file_format)
+
+        translated_text = []
+        for key in dict:
+            rr = dict[key]
+            LOG.info(rr)
+            translated_text.append(dict[key])
+        return translated_text
